992169c3de7e7632e6b5b4be752824dc98e55fa2
Nazar Potipaka <mutado.nzr@gmail.com>
2024-04-23 20:22:55 +0200

    feat: add front-end
    added vue, eslint, ts, tailwind, prettier

---
+++ resources/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        nav
+    </nav>
+    <main>
+        <router-view></router-view>
+    </main>
+    <footer>
+        footer
+    </footer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# direct all request to app.blade
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
