bd84a126658f83c1a8cb16eec19536feb51c5abd
Somebody1369 <anastasiia.kravt@gmail.com>
2024-05-01 17:39:06 +0300

    create icons from svg, cleanup and tabs

---
+++ resources/components/ProfileTab.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex flex-col gap-8">
+    <div class="flex flex-col gap-3 text-left items-start w-full">
+      <h5 class="text-sm text-neutral-700 font-bold">About</h5>
+      <p class="text-neutral-950 text-sm font-normal">{{ user.about }}</p>
+    </div>
+
+    <div class="flex flex-col gap-3 text-left items-start w-full">
+      <h5 class="text-sm text-neutral-700 font-bold">Writing goals</h5>
+      <ul>
+        <li
+          v-for="(goal, goalID) in user.goals"
+          :key="goalID"
+          class="text-neutral-950 text-sm font-normal flex items-center gap-2 pl-2"
+        >
+          <dot-icon />
+          {{ goal }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex flex-col gap-3 text-left items-start w-full">
+      <h5 class="text-sm text-neutral-700 font-bold">Inspired by</h5>
+      <div class="flex item-start gap-3">
+        <image-component
+          v-for="(poster, posterID) in user.inspiration"
+          :key="posterID"
+          :src="poster.image?.path"
+          alt="poster"
+          class="rounded-lg object-cover w-24 h-36 shrink-0"
+        />
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-3 text-left items-start w-full">
+      <h5 class="text-sm text-neutral-700 font-bold">Favorite characters</h5>
+      <div class="flex gap-2 items-center flex-wrap">
+        <p
+          v-for="(character, characterID) in user.favorite_characters"
+          :key="characterID"
+          class="text-slate-600 text-xs font-normal px-3 py-2 rounded-full"
+          style="background-color: rgba(96, 159, 255, 0.1)"
+        >
+          {{ character }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ImageComponent from '@/components/ImageComponent.vue'
+
+const props = defineProps({
+  user: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped></style>
--- resources/views/OnboardingSummary.vue
+++ resources/views/OnboardingSummary.vue
@@ -1,56 +1,7 @@
 <template>
   <div class="w-full min-h-screen flex flex-col items-center pt-10 pb-8 px-4">
     <template v-if="loading">
-      <div class="flex flex-col items-center mb-[-129px]">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="61"
-          height="61"
-          viewBox="0 0 61 61"
-          fill="none"
-        >
-          <circle
-            cx="30.5"
-            cy="30.228"
-            r="30.228"
-            fill="url(#paint0_linear_203_749)"
-          />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M40.927 22.8836C43.0823 27.1195 44.8269 32.0616 44.1214 36.7816C43.0665 43.8188 37.3354 48.2294 30.8457 48.3942C19.8878 48.6725 14.5825 39.3798 17.5185 29.5357C18.2987 26.7108 20.1565 24.0371 22.1623 21.8445C23.0905 20.8416 23.6106 20.0599 23.7227 22.2899C23.7519 23.3627 24.1422 24.39 24.4714 25.0191C24.7264 25.5152 24.773 25.4062 25.1375 25.0191C27.9121 21.9928 29.2528 17.6639 30.1139 13.7451C30.5492 11.7638 30.5219 11.712 32.1197 12.6301C35.5156 14.5814 39.2087 19.5144 40.927 22.8836Z"
-            fill="white"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_203_749"
-              x1="0.271973"
-              y1="0"
-              x2="30.5"
-              y2="60.4561"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#FF9202" />
-              <stop
-                offset="1"
-                stop-color="#FD2D24"
-              />
-            </linearGradient>
-          </defs>
-        </svg>
-
-        <h1 class="text-black text-2xl font-bold">
-          Story
-          <span class="text-orange-500 -ml-1">Flame</span>
-        </h1>
-      </div>
-
-      <div class="mx-auto my-auto flex flex-col items-center gap-2">
-        <AppLoader />
-        <span class="text-black text-sm font-normal text-center">
-          Processing your data..
-        </span>
-      </div>
+      <loading-tab class="w-full min-h-screen" />
     </template>
 
     <template v-else>
@@ -61,18 +12,7 @@
           <div
             class="w-24 h-24 bg-gray-200 shrink-0 rounded-full flex items-center justify-center"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="61"
-              height="60"
-              viewBox="0 0 61 60"
-              fill="none"
-            >
-              <path
-                d="M30.5 6C27.3174 6 24.2652 7.26428 22.0147 9.51472C19.7643 11.7652 18.5 14.8174 18.5 18C18.5 21.1826 19.7643 24.2348 22.0147 26.4853C24.2652 28.7357 27.3174 30 30.5 30C33.6826 30 36.7348 28.7357 38.9853 26.4853C41.2357 24.2348 42.5 21.1826 42.5 18C42.5 14.8174 41.2357 11.7652 38.9853 9.51472C36.7348 7.26428 33.6826 6 30.5 6ZM15.527 33C14.7368 32.9964 13.9537 33.149 13.2226 33.4489C12.4915 33.7489 11.8269 34.1903 11.2669 34.7478C10.7069 35.3053 10.2625 35.9679 9.95929 36.6976C9.65608 37.4274 9.49999 38.2098 9.5 39C9.5 44.073 11.999 47.898 15.905 50.391C19.751 52.842 24.935 54 30.5 54C36.065 54 41.249 52.842 45.095 50.391C49.001 47.901 51.5 44.07 51.5 39C51.5 37.4087 50.8679 35.8826 49.7426 34.7574C48.6174 33.6321 47.0913 33 45.5 33H15.527Z"
-                fill="#9E9E9E"
-              />
-            </svg>
+            <user-icon class="text-neutral-400" />
           </div>
 
           <div class="flex flex-col items-center text-center gap-2">
@@ -90,53 +30,28 @@
           </div>
         </div>
 
-        <div class="flex flex-col gap-3 text-left items-start w-full">
-          <h5 class="text-sm text-neutral-700 font-bold">About</h5>
-          <p class="text-neutral-950 text-sm font-normal">{{ user.about }}</p>
+        <div
+          class="flex items-center gap-2 justify-between max-w-full overscroll-auto"
+        >
+          <button
+            v-for="(tab, tabID) in tabs"
+            :key="tabID"
+            :class="
+              activeTab == tabID
+                ? 'bg-neutral-800 text-pure-white'
+                : 'bg-gray-200 text-neutral-400'
+            "
+            class="rounded-full text-sm font-semibold whitespace-nowrap select-none py-1 px-3"
+            @click="activeTab = tabID"
+          >
+            {{ tab }}
+          </button>
         </div>
 
-        <div class="flex flex-col gap-3 text-left items-start w-full">
-          <h5 class="text-sm text-neutral-700 font-bold">Writing goals</h5>
-          <ul>
-            <li
-              v-for="(goal, goalID) in user.goals"
-              :key="goalID"
-              class="text-neutral-950 text-sm font-normal flex items-center gap-2 pl-2"
-            >
-              <dot-icon />
-              {{ goal }}
-            </li>
-          </ul>
-        </div>
-
-        <div class="flex flex-col gap-3 text-left items-start w-full">
-          <h5 class="text-sm text-neutral-700 font-bold">Inspired by</h5>
-          <div class="flex item-start gap-3">
-            <image-component
-              v-for="(poster, posterID) in user.inspiration"
-              :key="posterID"
-              :src="poster.image?.path"
-              alt="poster"
-              class="rounded-lg object-cover w-24 h-36 shrink-0"
-            />
-          </div>
-        </div>
-
-        <div class="flex flex-col gap-3 text-left items-start w-full">
-          <h5 class="text-sm text-neutral-700 font-bold">
-            Favorite characters
-          </h5>
-          <div class="flex gap-2 items-center flex-wrap">
-            <p
-              v-for="(character, characterID) in user.favorite_characters"
-              :key="characterID"
-              class="text-slate-600 text-xs font-normal px-3 py-2 rounded-full"
-              style="background-color: rgba(96, 159, 255, 0.1)"
-            >
-              {{ character }}
-            </p>
-          </div>
-        </div>
+        <profile-tab
+          v-if="activeTab == 0"
+          :user="user"
+        />
       </div>
     </template>
   </div>
@@ -144,9 +59,11 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import AppLoader from '@/components/AppLoader.vue'
+import LoadingTab from '@/components/LoadingTab.vue'
+import ProfileTab from '@/components/ProfileTab.vue'
+
 import DotIcon from '@/components/icons/DotIcon.vue'
-import ImageComponent from '@/components/ImageComponent.vue'
+import UserIcon from '@/components/icons/UserIcon.vue'
 
 const loading = ref(false)
 
@@ -180,5 +97,8 @@ const user = {
     'Dean Winchester',
   ],
 }
+
+const activeTab = ref(0)
+const tabs = ['My profile', 'My stories', 'My achievements']
 </script>
 <style scoped></style>
