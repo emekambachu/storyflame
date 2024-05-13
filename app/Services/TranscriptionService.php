f40621a312cb7700b95909cf4f187e813393e2ef
Nazar Potipaka <mutado.nzr@gmail.com>
2024-05-13 21:21:12 +0200

    Update TranscriptionService.php

--- app/Services/TranscriptionService.php
+++ app/Services/TranscriptionService.php
@@ -4,19 +4,22 @@ namespace App\Services;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class TranscriptionService
 {
     public function transcribe(string|UploadedFile $path_or_file): string
     {
         if ($path_or_file instanceof UploadedFile) {
-            $path_to_audio = $path_or_file->store('tmp');
+            $filename = now()->format('Y-m-d') . '-' . uniqid() . '.' . $path_or_file->getClientOriginalExtension();
+            Storage::disk('local')->put('audio/' . $filename, $path_or_file->get());
+            $path_to_audio = storage_path('app/audio/' . $filename);
         } else {
             $path_to_audio = $path_or_file;
         }
 
         $response = Http::post(config('app.processing_url') . '/transcribe', [
-            'path' => storage_path('app/' . $path_to_audio),
+            'path' => $path_to_audio,
         ]);
 
         $response->throw();
