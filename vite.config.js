38cabd43ea3c55cb24e7409ec5c3fabfc753a3ef
Mitch Lusas <mlusas@users.noreply.github.com>
2024-07-18 16:00:54 -0700

    Fixes to vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -3,71 +3,65 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+export default defineConfig(async() => {
+    const tailwindcss = await import('tailwindcss');
+    const autoprefixer = await import('autoprefixer');
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/assets/app.css',
-                'resources/app.ts',
-                'resources/admin-app.ts',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    // The Vue plugin will re-write asset URLs, when referenced
-                    // in Single File Components, to point to the Laravel web
-                    // server. Setting this to `null` allows the Laravel plugin
-                    // to instead re-write asset URLs to point to the Vite
-                    // server instead.
-                    base: null,
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/assets/app.css',
+                    'resources/app.ts',
+                    'resources/admin-app.ts',
+                ],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        // The Vue plugin will re-write asset URLs, when referenced
+                        // in Single File Components, to point to the Laravel web
+                        // server. Setting this to `null` allows the Laravel plugin
+                        // to instead re-write asset URLs to point to the Vite
+                        // server instead.
+                        base: null,
 
-                    // The Vue plugin will parse absolute URLs and treat them
-                    // as absolute paths to files on disk. Setting this to
-                    // `false` will leave absolute URLs un-touched so they can
-                    // reference assets in the public directory as expected.
-                    includeAbsolute: false,
+                        // The Vue plugin will parse absolute URLs and treat them
+                        // as absolute paths to files on disk. Setting this to
+                        // `false` will leave absolute URLs un-touched so they can
+                        // reference assets in the public directory as expected.
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
-    css: {
-        postcss: {
-            plugins: [
-                tailwind(),
-                autoprefixer(),
-            ],
-        },
-    },
-    resolve: {
-        alias: {
-            // eslint-disable-next-line no-undef
-            '@': resolve(__dirname, 'resources'),
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
-    css: {
-        postcss: {
-            plugins: [
-                require('tailwindcss'),
-                require('autoprefixer'),
-            ],
-        },
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return 'vendor';
-                    }
-                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                // eslint-disable-next-line no-undef
+                '@': resolve(__dirname, 'resources'),
+                vue: 'vue/dist/vue.esm-bundler.js',
             },
         },
-        chunkSizeWarningLimit: 1600,
-    },
+        css: {
+            postcss: {
+                plugins: [
+                    tailwindcss.default,
+                    autoprefixer.default,
+                ],
+            },
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                    },
+                },
+            },
+            chunkSizeWarningLimit: 1600,
+        },
+    }
 })
